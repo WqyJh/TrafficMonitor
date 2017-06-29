b09fadb448e9e36093af5998785f85b04767dea0
WqyJh <781345688@qq.com>
2017-06-29 23:01:39 +0800

    取消客户端项目的预编译头，跑通单元测试项目

---
+++ TMClientUnitTest/UploaderTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Uploader.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TMClientUnitTest 
+{
+	TEST_CLASS(UploaderTest)
+	{
+	public:
+		TEST_METHOD(test_upload)
+		{
+			Uploader uploader;
+			Assert::AreEqual(0, uploader.upload("filepath", "filename"));
+		}
+	};
+}
