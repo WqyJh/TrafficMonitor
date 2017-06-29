93876b4f6232f29e91c5b35f5c27bbf85c97338c
Qiying Wang <Qiying Wang>
2017-06-29 19:16:25 +0800

    添加项目文件。

---
+++ TrafficMonitor/TrafficMonitor.h
@@ -0,0 +1,32 @@
+
+// TrafficMonitor.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTrafficMonitorApp: 
+// 有关此类的实现，请参阅 TrafficMonitor.cpp
+//
+
+class CTrafficMonitorApp : public CWinApp
+{
+public:
+	CTrafficMonitorApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTrafficMonitorApp theApp;
