2edf5a09f0aae4a832dcc8d08555159f7dadda66
WqyJh <781345688@qq.com>
2017-06-30 10:17:10 +0800

    添加PlateDao的注释

--- TMServer/PlateDao.h
+++ TMServer/PlateDao.h
@@ -28,12 +28,41 @@ public:
 	*/
 	int remove(const Plate &plate);
 
+	/*
+	* Summary: 更新一个Plate实例对应的数据项
+	* Parameters:
+	*		plate: 一个Plate实例
+	* Return:
+	*		0表示成功，非0表示失败
+	*/
 	int update(const Plate &plate);
 
-	Plate &findById(int id);
+	/*
+	* Summary: 按id查询一个数据
+	* Parameters:
+	*		id: 数据项的id
+	* Return:
+	*		如果查询成功，返回一个指向一个Plate实例的指针，失败则返回 nullptr
+	*/
+	Plate *findById(int id);
 
+	/*
+	* Summary: 按车牌号查询数据
+	* Parameters:
+	*		number: 表示车牌号的字符串
+	* Return:
+	*		如果查询成功，返回一组符合条件的Plate的指针，失败则返回一个长度为0的向量。
+	*/
 	std::vector<Plate *> &findByNumber(std::string number);
 
+	/*
+	* Summary: 按时间查询数据
+	* Parameters:
+	*		time: 表示时间的字符串，以天为单位，格式为 "yy-mm-dd"，
+	*				y表示年，m表示月，d表示日。
+	* Return:
+	*		如果查询成功，返回一组符合条件的Plate的指针，失败则返回一个长度为0的向量。
+	*/
 	std::vector<Plate *> findByTime(std::string time);
 };
 
