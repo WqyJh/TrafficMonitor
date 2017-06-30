af2af33e72743532d5811c4bd97f5b4b123dc3c9
Kyoy <183598258@qq.com>
2017-06-30 11:19:14 +0800

    修改了plate.cpp

--- TMServerUnitTest/PlateTest.cpp
+++ TMServerUnitTest/PlateTest.cpp
@@ -5,15 +5,21 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TMServerUnitTest
-{		
+{
 	TEST_CLASS(TestPlate)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(test_plate)
 		{
 			Plate p(1, "123", 20170630, "D:\\vs-c");
 			Assert::AreEqual(1, p.get_id());
+
+			std::string str1("1234");
+			p.set_number(str1);
+			p.set_id(2);
+			Assert::AreEqual(2, p.get_id());
+			Assert::AreEqual(str1, p.get_number());
 			// TODO: 在此输入测试代码
 		}
 
