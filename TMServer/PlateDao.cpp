2028035ebfdd661f9e4477c72c89564d8a6b97d0
Kyoy <183598258@qq.com>
2017-07-01 21:42:29 +0800

    优化了代码格式

--- TMServer/PlateDao.cpp
+++ TMServer/PlateDao.cpp
@@ -2,20 +2,15 @@
 #include "PlateDao.h"
 #include <iostream>
 
-
-//char* gszFile = "D:\\PlateForDao.db";
-
-//PlateDao::PlateDao(char* path)
-//{
-//	gszFile = path;
-//	CppSQLite3DB db;
-//	db.open(gszFile);
-//	if (!db.tableExists("Plate"))
-//	{
-//		db.execDML("CREATE TABLE Plate(_id integer primary key, number char(20), time int(8), path varchar(255))");
-//		db.execDML("CREATE INDEX numIndex ON plate(number)");
-//	}
-//}
+Plate* getTable(CppSQLite3Table &t)
+{
+	Plate* p = new Plate;
+	p->set_id(atoi(t.fieldValue(0)));
+	p->set_number(t.fieldValue(1));
+	p->set_time(atol(t.fieldValue(2)));
+	p->set_path(t.fieldValue(3));
+	return p;
+}
 
 PlateDao::PlateDao(char* path)
 {
@@ -43,9 +38,6 @@ void PlateDao::release()
 
 int PlateDao::save(const Plate& plate)
 {
-
-	//CppSQLite3DB db;
-	//db.open(gszFile);
 	std::string num = plate.get_number();
 	const char * number = num.c_str();
 	long time = plate.get_time();
@@ -54,7 +46,6 @@ int PlateDao::save(const Plate& plate)
 	CppSQLite3Buffer bufSQL;
 	bufSQL.format("INSERT INTO Plate(number, path, time) VALUES(%Q, %Q, %ld)", number, path, time);
 	int flag = db.execDML(bufSQL);
-	//int flag = db.execDML(buf);
 	if (flag == 1)
 		return 0;
 	return 1;
@@ -62,15 +53,6 @@ int PlateDao::save(const Plate& plate)
 
 int PlateDao::remove(const Plate& plate)
 {
-	/*CppSQLite3DB db;
-	db.open(gszFile);*/
-	//std::string number = plate.get_number();
-	//std::string num = plate.get_number();
-	//const char * number = num.c_str();
-	//long time = plate.get_time();
-	//std::string path = plate.get_path();
-	//std::string pa = plate.get_path();
-	//const char * path = pa.c_str();
 	int id = plate.get_id();
 	CppSQLite3Buffer bufSQL;
 	bufSQL.format("DELETE FROM Plate WHERE _id=%d", id);
@@ -82,14 +64,10 @@ int PlateDao::remove(const Plate& plate)
 
 int PlateDao::update(const Plate& plate)
 {
-	/*CppSQLite3DB db;
-	db.open(gszFile);*/
 	int id = plate.get_id();
-	//std::string number = plate.get_number();
 	std::string num = plate.get_number();
 	const char * number = num.c_str();
 	long time = plate.get_time();
-	//std::string path = plate.get_path();
 	std::string pa = plate.get_path();
 	const char * path = pa.c_str();
 	CppSQLite3Buffer bufSQL;
@@ -100,20 +78,18 @@ int PlateDao::update(const Plate& plate)
 	return 1;
 }
 
-Plate* getTable(CppSQLite3Table &t)
-{
-	Plate* p = new Plate;
-	p->set_id(atoi(t.fieldValue(0)));
-	p->set_number(t.fieldValue(1));
-	p->set_time(atol(t.fieldValue(2)));
-	p->set_path(t.fieldValue(3));
-	return p;
-}
+//Plate* getTable(CppSQLite3Table &t)
+//{
+//	Plate* p = new Plate;
+//	p->set_id(atoi(t.fieldValue(0)));
+//	p->set_number(t.fieldValue(1));
+//	p->set_time(atol(t.fieldValue(2)));
+//	p->set_path(t.fieldValue(3));
+//	return p;
+//}
 
 Plate* PlateDao::findById(int id)
 {
-	/*CppSQLite3DB db;
-	db.open(gszFile);*/
 	CppSQLite3Buffer bufSQL;
 	bufSQL.format("SELECT * FROM plate WHERE _id=%d",id);
 	CppSQLite3Table t = db.getTable(bufSQL);
@@ -122,12 +98,6 @@ Plate* PlateDao::findById(int id)
 	else
 	{
 		t.setRow(0);
-		/*Plate* p = new Plate;
-		p->set_id(atoi(t.fieldValue(0)));
-		p->set_number(t.fieldValue(1));
-		p->set_time(atol(t.fieldValue(2)));
-		p->set_path(t.fieldValue(3));
-		return p;*/
 		return getTable(t);
 	}
 }
@@ -137,8 +107,6 @@ std::vector<Plate *> PlateDao:: findByNumber(std::string number)
 	int row;
 	std::vector<Plate *> v;
 	const char* num = number.c_str();
-	/*CppSQLite3DB db;
-	db.open(gszFile);*/
 	CppSQLite3Buffer bufSQL;
 	bufSQL.format("SELECT * FROM Plate WHERE number=%Q", num);
 	CppSQLite3Table t = db.getTable(bufSQL);
@@ -148,11 +116,6 @@ std::vector<Plate *> PlateDao:: findByNumber(std::string number)
 		for (row = 0; row < t.numRows(); ++row)
 		{
 			t.setRow(row);
-			/*Plate* p = new Plate;
-			p->set_id(atoi(t.fieldValue(0)));
-			p->set_number(t.fieldValue(1));
-			p->set_time(atol(t.fieldValue(2)));
-			p->set_path(t.fieldValue(3));*/
 			Plate* p = getTable(t);
 			v.push_back(p);
 		}
@@ -164,11 +127,6 @@ std::vector<Plate *> PlateDao::findByTime(std::string time)
 	int row;
 	std::vector<Plate *> v;
 	const char* ti = time.c_str();
-	/*CppSQLite3DB db;
-	db.open(gszFile);*/
-	//CppSQLite3Buffer bufSQL;
-	//bufSQL.format("SELECT * FROM Plate WHERE date(time, 'unixepoch')==date(%Q)",ti);
-	//CppSQLite3Table t = db.getTable(bufSQL);
 	char buf[128];
 	sprintf_s(buf, "SELECT * FROM Plate WHERE date(time, 'unixepoch')==date(\'%s\')", ti);
 	CppSQLite3Table t = db.getTable(buf);
@@ -178,11 +136,6 @@ std::vector<Plate *> PlateDao::findByTime(std::string time)
 		for (row = 0; row < t.numRows(); ++row)
 		{
 			t.setRow(row);
-			/*Plate* p = new Plate;
-			p->set_id(atoi(t.fieldValue(0)));
-			p->set_number(t.fieldValue(1));
-			p->set_time(atol(t.fieldValue(2)));
-			p->set_path(t.fieldValue(3));*/
 			Plate* p = getTable(t);
 			v.push_back(p);
 		}
@@ -193,8 +146,6 @@ std::vector<Plate *> PlateDao::findAll()
 {
 	int row;
 	std::vector<Plate *> v;
-	/*CppSQLite3DB db;
-	db.open(gszFile);*/
 	CppSQLite3Table t = db.getTable("SELECT * FROM Plate");
 	if (t.numFields() == 0)
 		return v;
@@ -202,13 +153,9 @@ std::vector<Plate *> PlateDao::findAll()
 		for (row = 0; row < t.numRows(); ++row)
 		{
 			t.setRow(row);
-			/*Plate* p = new Plate;
-			p->set_id(atoi(t.fieldValue(0)));
-			p->set_number(t.fieldValue(1));
-			p->set_time(atol(t.fieldValue(2)));
-			p->set_path(t.fieldValue(3));*/
 			Plate* p = getTable(t);
 			v.push_back(p);
 		}
 	return v;
 }
+
--- TMServer/PlateDao.h
+++ TMServer/PlateDao.h
@@ -12,7 +12,7 @@ private:
 	char *gszFile;  //数据库路径 格式如"D:\\vs-c\\test.db"
 	CppSQLite3DB db;
 public:
-	void init();
+	void init(); //打开数据库 创建table
 	void release();
 	/*
 	*实例化PlateDao对象时新建数据库以及table
